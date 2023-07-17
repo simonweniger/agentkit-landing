1c70464b6720c9d6818eaa8c9832aa4e77a43e4c
Simon Weniger <simonweniger.sw@gmail.com>
2023-07-17 16:09:20 +0200

    add metadta

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -17,6 +17,18 @@ export default function App({ Component, pageProps }) {
           name="description"
           content="Build, deploy, and manage LLM-powered agents on  platforms"
         />
+        <meta property="og:type" content="website" />
+        <meta property="og:url" content="https://superagent-swift.vercel.app" />
+        <meta property="og:title" content="Superagent Swift" />
+        <meta property="og:description" content="Build, deploy, and manage LLM-powered agents on  platforms" />
+        <meta property="og:image" content="/create-agent.jpeg" />
+
+        {/* Twitter Card tags */}
+        <meta name="twitter:card" content="This is a community-made SwiftSDK for the Superagen LLM Framework. You need to have a superagent API Key in order to use the SDK. To get your API key head over to superagent.sh or fork the main repo and self-host it." />
+        <meta name="twitter:url" content="https://superagent-swift.vercel.app" />
+        <meta name="twitter:title" content="Superagent Swift" />
+        <meta name="twitter:description" content="Build, deploy, and manage LLM-powered agents on  platforms" />
+        <meta name="twitter:image" content="/create-agent.jpeg" />
       </Head>
       <ThemeProvider attribute="class" disableTransitionOnChange>
         <MDXProvider components={mdxComponents}>
